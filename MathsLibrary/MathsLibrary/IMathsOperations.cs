6ece46c219b406540931451baf8bc41531b09ba4
GREAUX Thomas <thomas.greaux@etu.unice.fr>
2018-03-01 10:16:38 +0100

    MathsLibrary initialization

---
+++ MathsLibrary/MathsLibrary/IMathsOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MathsLibrary
+{
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        int Add(int num1, int num2);
+        [OperationContract]
+        int Multiply(int num1, int num2);
+    }
+}
