be5bc59c4192095d49e01f0a621c1e7998d876eb
Thomas-Greaux <thomas.greaux@etu.unice.fr>
2018-04-05 10:01:39 +0100

    Created a self hosting service

--- WS_RS_Console/EventsClient/Program.cs
+++ WS_RS_Console/EventsClient/Program.cs
@@ -10,6 +10,7 @@ namespace EventsClient
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
---
+++ WS_RS_Console/EventsServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace EventsServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(WS_RS_VelibLabServices.VelibLabServices));
+                host.Open();
+                Console.WriteLine("Service hosted at ");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception : " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
